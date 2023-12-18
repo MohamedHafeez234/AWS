30c85df41a75648882cd5f732ee0466c724d6b1f
Mohamed Hafeez <47528990+MohamedHafeez234@users.noreply.github.com>
2023-12-18 15:31:35 +0530

    Update execute.py

--- Workflows/stage-1/execute.py
+++ Workflows/stage-1/execute.py
@@ -1,24 +1,21 @@
 import sys, os
 
 workflow_script_path = os.getenv("WORKER_SCRIPT_PATH", "workflow-scripts/")
-sys.path.append(workflow_script_path + "database/")
+sys.path.append(workflow_script_path + "database/rds")
 
 import json
 import boto3
-from common import LoggerSetup
-
-logger = LoggerSetup(loggerName=str(__file__)).getLogger()
-
+logger = LoggerSetup(loggerName=str(__file__), loggingLevel="debug").getLogger()
 from common.logger import LoggerSetup
 from common.utilities import (
     getinput,
     create_read_replica,
-    promote_read_replica
+    promote_read_replica,
 )
 
 def handler(input, *args):
     try:
-        db_instance_identifier, region,target_engine_version, new_parameter_group_name,replica_db_identifier = getinput(input)
+        db_instance_identifier,region,target_engine_version, new_parameter_group_name,replica_db_identifier,dns_record_name= getinput(input)
         stage_input = json.loads(args[0])
         if stage_input["Result"] == True:
             response=create_read_replica(db_instance_identifier,replica_db_identifier,region)
@@ -27,7 +24,7 @@ def handler(input, *args):
                 if promote:
                     return {
                         "output": {
-                            "success": "SUCCESSFUL",
+                            "success": "SUCCESSFULL",
                             "message": "Promotion of Read Replica is successful.",
                         },
                         "nextStageInput": {"Result": True},
@@ -41,16 +38,12 @@ def handler(input, *args):
                         "nextStageInput": {"Result": False},
                     }
 
-
     except Exception as error:
         logger.error(error, exc_info=True)
         return {
             "output": {
                 "success": "FAILED",
-                "message": "Caught an exception.",
+                "message": "error : " + str(error),
             },
             "nextStageInput": {"Result": False},
         }
-
-
-
