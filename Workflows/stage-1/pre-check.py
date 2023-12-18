f7ea467878946ebdfd0c135822d1cf6e288f2ab8
Mohamed Hafeez <47528990+MohamedHafeez234@users.noreply.github.com>
2023-12-18 15:29:27 +0530

    Update pre-check.py

--- Workflows/stage-1/pre-check.py
+++ Workflows/stage-1/pre-check.py
@@ -1,66 +1,47 @@
-# Facing issue with importing common package, added the current execution path(python3 stage-1/pre-check.py) to sys.path
 import sys, os
 
 workflow_script_path = os.getenv("WORKER_SCRIPT_PATH", "workflow-scripts/")
-sys.path.append(workflow_script_path + "database/")
-
-import json
-
+sys.path.append(workflow_script_path + "database/rds")
+logger = LoggerSetup(loggerName=str(__file__), loggingLevel="debug").getLogger()
 from common.logger import LoggerSetup
 from common.utilities import (
     getinput,
     get_rds_instance_details,
-    is_max_replicas_reached,
     check_read_replica_present,
 )
 
-logger = LoggerSetup(loggerName=str(__file__), loggingLevel="debug").getLogger()
-
 def handler(input, *args):
     try:
-        input = json.loads(input)
-        db_instance_identifier, region,target_engine_version, new_parameter_group_name,replica_db_identifier = getinput(input)
-        db_instance_name = db_instance_identifier
-        db_instance_details=get_rds_instance_details(db_instance_name,region)
+        db_instance_identifier,region,target_engine_version, new_parameter_group_name,replica_db_identifier,dns_record_name= getinput(input)
+        db_instance_details=get_rds_instance_details(db_instance_identifier)
         current_running_version=db_instance_details['EngineVersion']
         Engine=db_instance_details['Engine']
         target_version = target_engine_version
-        if is_max_replicas_reached(db_instance_name,region):
-            logger.info(f"Max Replicas count reached `{db_instance_name}")
-            if check_read_replica_present(db_instance_identifier,replica_db_identifier, region):
-                logger.error(f"Read Replicas already present `{replica_db_identifier}")
-                return {
-                    "output": {
-                        "success": "FAILED",
-                        "message": "Read Replicas already present",
-                    },
-                    "nextStageInput": {"Result": False},
-                }
-            else:
-                logger.info(f"Read Replicas not present `{replica_db_identifier}")
-                return {"output": {
-                        "success": "Success",
-                        "message": "Read Replica not working ",
-                    },
-                    "nextStageInput": {"Result": True},
-                }
-        
-        else:
-            logger.error(f"Max Replicas count reached `{db_instance_name}")
+        if check_read_replica_present(db_instance_identifier,replica_db_identifier):
+            logger.error(f"Read Replicas already present `{replica_db_identifier}")
             return {
-                    "output": {
-                        "success": "FAILED",
-                        "message": "Max Replicas count reached",
-                    },
-                    "nextStageInput": {"Result": False},
-                }
+                "output": {
+                    "success": "FAILED",
+                    "message": "Read Replicas already present",
+                },
+                "nextStageInput": {"Result": False},
+            }
+        else:
+            logger.info(f"Read Replicas not present `{replica_db_identifier}")
+            return {
+                "output": {
+                    "success": "SUCCESSFULL",
+                    "message": "Read Replica not present `{replica_db_identifier}` ",
+                },
+                "nextStageInput": {"Result": True},
+            }
 
     except Exception as error:
         logger.error(error, exc_info=True)
         return {
             "output": {
                 "success": "FAILED",
-                "message": "Caught an exception!",
+                "message": "error : " + str(error),
             },
             "nextStageInput": {"Result": False},
         }
