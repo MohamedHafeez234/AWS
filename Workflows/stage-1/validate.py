dc3892ed28d36b866a808f61c23eaf6fbc557cfd
Mohamed Hafeez <47528990+MohamedHafeez234@users.noreply.github.com>
2023-12-18 15:32:06 +0530

    Update validate.py

--- Workflows/stage-1/validate.py
+++ Workflows/stage-1/validate.py
@@ -2,58 +2,56 @@
 import sys, os
 
 workflow_script_path = os.getenv("WORKER_SCRIPT_PATH", "workflow-scripts/")
-sys.path.append(workflow_script_path + "database/")
-
+sys.path.append(workflow_script_path + "database/rds")
 import json
-
+logger = LoggerSetup(loggerName=str(__file__), loggingLevel="debug").getLogger()
 from common.logger import LoggerSetup
 from common.utilities import (
     getinput,
     wait_until_instance_active,
     validate_db_creation,
     )
-logger = LoggerSetup(loggerName=str(__file__), loggingLevel="debug").getLogger()
 
 def handler(input, *args):
     try:
-        input = json.loads(input)
-        db_instance_identifier, region,target_engine_version, new_parameter_group_name,replica_db_identifier = getinput(input)
+        db_instance_identifier,region,target_engine_version,new_parameter_group_name,replica_db_identifier,dns_record_name= getinput(input)
+        stage_input = json.loads(args[0])
         while True:
-            response_message=wait_until_instance_active(db_instance_identifier,region,sleep_interval=10)
+            response_message=wait_until_instance_active(db_instance_identifier)
             if response_message == True:
                 logger.info(f"Instance -  {db_instance_identifier} is Active.")
                 break
-        stage_input = json.loads(args[0])
+        while True:
+            response_message1=wait_until_instance_active(replica_db_identifier)
+            if response_message1 == True:
+                logger.info(f"Instance -  {replica_db_identifier} is Active.")
+                break
         if stage_input["Result"] == True:
-            response=validate_db_creation(replica_db_identifier,region)
-            if response:
-                return {
-                    "output": {
-                        "success": "SUCCESSFUL",
-                        "message": "Read Replica instance is standalone.",
-                    },
-                    "nextStageInput": {"Result": True},
-                }
-            else:
-                return {
-                    "output": {
-                        "success": "Failed",
-                        "message": "Read Replica instance is not standalone.",
-                    },
-                    "nextStageInput": {"Result": False},
-                }
-
+                response=validate_db_creation(replica_db_identifier)
+                if response:
+                    return{
+                        "output": {
+                            "success": "SUCCESSFULL",
+                            "message": "Read Replica instance is standalone.",
+                        },
+                        "nextStageInput": {"Result": True},
+                    }
+                    
+                else:
+                    return{
+                        "output": {
+                            "success": "INPROGRESS",
+                            "message": "Read Replica instance is not standalone.",
+                        },
+                        "nextStageInput": {"Result": False},
+                    }
 
     except Exception as error:
         logger.error(error, exc_info=True)
-        return {
+        return{
             "output": {
                 "success": "FAILED",
-                "message": "Caught an exception.",
+                "message": "error : " + str(error),
             },
             "nextStageInput": {"Result": False},
         }
-
-
-
-
